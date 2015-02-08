810d68cea9317a68aad608f6d27981dd794cfc62
skyboy <skyboy026@gmail.com>
2015-02-08 01:13:00 -0500

    Fix inventory not associating dropped item with player

--- src/cofh/scoring/LoadingPlugin.java
+++ src/cofh/scoring/LoadingPlugin.java
@@ -12,6 +12,7 @@ import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.InsnNode;
 import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.VarInsnNode;
@@ -42,12 +43,37 @@ public class LoadingPlugin implements IFMLLoadingPlugin, IClassTransformer {
 				}
 			}
 
-			for (AbstractInsnNode n = onAttack.instructions.getFirst(); n.getNext() != null; n = n.getNext()) {
+			for (AbstractInsnNode n = onAttack.instructions.getFirst(); n != null; n = n.getNext()) {
 				if (n.getOpcode() == INVOKEVIRTUAL && setDead.equals(((MethodInsnNode) n).name)) {
 					onAttack.instructions.insertBefore(n, new VarInsnNode(ALOAD, 0));
 					onAttack.instructions.insertBefore(n, new VarInsnNode(ALOAD, 1));
 					onAttack.instructions.insertBefore(n, new MethodInsnNode(INVOKESTATIC, "cofh/scoring/Scoring", "itemDeath",
 							"(Lnet/minecraft/entity/item/EntityItem;Lnet/minecraft/util/DamageSource;)V", false));
+				}
+			}
+
+			ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+			cn.accept(cw);
+			bytes = cw.toByteArray();
+		} else if ("net.minecraft.inventory.Container".equals(transformedName)) {
+			ClassNode cn = new ClassNode();
+			new ClassReader(bytes).accept(cn, 0);
+			String method = "func_71019_a";
+			if (!runtimeDeobfEnabled) {
+				method = "dropPlayerItemWithRandomChoice";
+			}
+
+			for (MethodNode m : cn.methods) {
+				for (AbstractInsnNode n = m.instructions.getFirst(); n != null; n = n.getNext()) {
+					if (n.getOpcode() == INVOKEVIRTUAL && method.equals(((MethodInsnNode) n).name)) {
+						m.instructions.remove(n.getPrevious());
+						m.instructions.insertBefore(n, new InsnNode(ICONST_1));
+						m.instructions.insertBefore(n, new MethodInsnNode(INVOKESTATIC, "net/minecraftforge/common/ForgeHooks",
+								"onPlayerTossEvent", "(Lnet/minecraft/entity/player/EntityPlayer;" +
+										"Lnet/minecraft/item/ItemStack;Z)Lnet/minecraft/entity/item/EntityItem;", false));
+						n = n.getPrevious();
+						m.instructions.remove(n.getNext());
+					}
 				}
 			}
 
